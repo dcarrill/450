e163188dd0473ade47abb951ceeb6631b7c383a3
dcarrill <dcarri6@uic.edu>
2017-02-20 11:55:54 -0600

    Stupid Tests

--- war.py
+++ war.py
@@ -18,6 +18,7 @@ for each game which contain the game's state, for instance things like the
 socket, the cards given, the cards still available, etc.
 """
 Game = namedtuple("Game", ["p1", "p2"])
+PLAYERS = asyncio.Queue(maxsize=2)
 
 class Command(Enum):
     """
@@ -42,21 +43,15 @@ def readexactly(sock, numbytes):
     Accumulate exactly `numbytes` from `sock` and return those. If EOF is found
     before numbytes have been received, be sure to account for that here or in
     the caller.
-
-
-    Have yet to account for EOF error**************************************************
     """
     bytes_recv = b''
     while numbytes != len(bytes_recv):
         bytes_recv += sock.recv(numbytes - len(bytes_recv))
     return bytes_recv
 
-#changed the arguments here************************************************************
 def kill_game(player_list):
     """
-    TODO: If either client sends a bad message, immediately nuke the game.
-
-    Am I also nuking the server's socket??? ******************************************
+    Closes the player (client) connections on error.
     """
     player_list[0].close()
     player_list[1].close()
@@ -80,7 +75,6 @@ def compare_cards(card1, card2):
 
 def deal_cards():
     """
-    TODO:
     1. create a deck of 0 - 51 cards - list of bytes?
     2. randomize the deck
     3. split the deck in half
@@ -95,114 +89,115 @@ def deal_cards():
     player_hands.append(bytes(second_half))
     return player_hands
 
-def serve_game(host, port):
+def serve_game(reader, writer):
     """
-    TODO: Open a socket for listening for new connections on host:port, and
-    perform the war protocol to serve a game of war between each client.
-    This function should run forever, continually serving clients.
-
-
-    Threading: 
-    ~~~~~~~~~
-    I know I will need:
-    t.Thread(func, args_to_func)
-    t.start() -->RuntimeError if called more than once on the same thread object.
-    t.join() --> RuntimeError if joining a thread that causes deadlock or trying to access before it's started
+    Create task to handle initial client connection
     """
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.bind((host, port))
-        sock.listen(2)
-        player1, addr = sock.accept()
-        player2, addr2 = sock.accept()
-        play1res = readexactly(player1, 2)
-        play2res = readexactly(player2, 2)
-        #Make sure first request from players is 'want game'
-        if play1res != b'\0\0' or play2res != b'\0\0':
-            print("Initial command was not 'want game'")
+    player_task = asyncio.Task(start_player(reader, writer))
+    print("after task assign")
+    try:
+        PLAYERS.put_nowait(player_task)
+    except asyncio.QueueFull:
+        logging.error("Queue is full...")
+    #If we have two players, let's launch a game.
+    if PLAYERS.qsize() == 2:
+        print("We have two connected clients!")
+        game_play()
+
+async def start_player(reader, writer):
+    """
+    Get first response from Player, make sure they want to play game.
+    """
+    response = await reader.read(2)
+    print("response or something")
+    print(reader)
+    if response != b'\0\0':
+        logging.debug("If you don't want to play, why are you here?")
+    return (reader, writer)
+
+def game_play():
+    print("inside gameplay")
+    """ Play the game
+    'Pop' the first two clients off the list
+    """
+    player1 = PLAYERS.get() #format: (reader, writer)
+    player2 = PLAYERS.get()
+    #Both players pop off - their first read occurs
+    print("Players got popped off")
+
+"""
+    #begin gameplay
+    #get their initial responses
+    play1res = player1.read(2)
+    play2res = player2.read(2)
+
+    #Make sure first request from players is 'want game'
+    if play1res != b'\0\0' or play2res != b'\0\0':
+        logging.debug("Didn't really want to play the game, huh?")
+        kill_game([player1, player2])
+        return
+
+    #Otherwise the players sent a valid intial responses
+    #Deal player hands
+    player_hands = deal_cards()
+    player1_hand = player_hands[0]
+    player2_hand = player_hands[1]
+
+    #Check for server errors
+    numhands = len(player_hands)
+    numcards1 = len(player_hands[0])
+    numcards2 = len(player_hands[1])
+    #Server did not give them the correct number of cards
+    if numhands != 2 or numcards1 != 26 or numcards2 != 26:
+        logging.debug("Server's Mistake in dealing!")
+        kill_game([player1, player2])
+        return
+    #Dealing hand to each - start of game
+    player1.writer.write(b'\1' + player_hands[0])
+    player2.writer.write(b'\1' + player_hands[1])
+
+    #Card comparisons
+    game_round = 0
+    while game_round < 26:
+        play1res = player1.reader.read(2)
+        play2res = player2.reader.read(2)
+
+        #Make sure their request is 'play card'
+        if play1res[0:1] != b'\2' or play2res[0:1] != b'\2':
+            logging.debug("Player did not send 'Play Card' command")
             kill_game([player1, player2])
             return
-        """
-        The following chunk of code (up the asterisk line) is what I think should be a new function (game_play).
-        Only arguments needed is the player sockets --> could be single argument, list.
-        Basic idea (for creating a single thread)
 
-        while <cond>:
-            <<listen...accept*2
-            get first request from each client >>
-            
-            if both requests are NOT  "want game"
-                kill everything
-            #otherwise, create new thread
-            t = Thread(game_play, [player1, player2])
-            t.start()
-            t.join()
-            <return from serve_game>
+        p1_card = play1res[1]
+        p2_card = play2res[1]
 
-        Questions:
-        >>1) Not within context: Do I need "addr/addr2" for anything? 
-            1.1) Also, using "with" means I don't have to call "sock.close()"??
-        >>2) Can I use the .listen(), .accept() over and over again in a loop like that?
-        >>3) If I spawn off threads, can my server still send/receive responses/requests from the clients in those threads?
-        >>4) I don't HAVE to define a new subclass for the Thread class, do I? 
-        """
-        player_hands = deal_cards()
-        player1_hand = player_hands[0]
-        player2_hand = player_hands[1]
-
-        numhands = len(player_hands)
-        numcards1 = len(player_hands[0])
-        numcards2 = len(player_hands[1])
-
-        #I did not give them the correct number of cards
-        if numhands != 2 or numcards1 != 26 or numcards2 != 26:
-            logging.debug("Hands: %d\tP1 cards: %d\tP2 cards: %d", numhands, numcards1, numcards2)
+        #card is NOT in their hands
+        if player1_hand.find(p1_card) == -1 or player2_hand.find(p2_card) == -1:
+            logging.debug("Played a card not in your hand")
             kill_game([player1, player2])
             return
-        #Dealing hand to each - start of game
-        player1.send(b'\1' + player_hands[0])
-        player2.send(b'\1' + player_hands[1])
 
-        game_round = 0
-        while game_round < 26:
-            play1res = readexactly(player1, 2)
-            play2res = readexactly(player2, 2)
+        player1_hand.replace(bytes([p1_card]), b' ')
+        player2_hand.replace(bytes([p2_card]), b' ')
 
-            #Make sure their request is 'play card'
-            if play1res[0:1] != b'\2' or play2res[0:1] != b'\2':
-                print("Player did not send 'Play Card' command")
-                kill_game([player1, player2])
-                return
-
-            p1_card = play1res[1]
-            p2_card = play2res[1]
-
-            #card is NOT in their hands
-            if player1_hand.find(p1_card) == -1 or player2_hand.find(p2_card) == -1:
-                print("Played a card not in your hand")
-                kill_game([player1, player2])
-                return
-
-            player1_hand.replace(bytes([p1_card]), b' ')
-            player2_hand.replace(bytes([p2_card]), b' ')
-
-            result = compare_cards(p1_card, p2_card)
-            if result == 0:
-                player1.send(b'\3\1')
-                player2.send(b'\3\1')
-            elif result == -1: #player 1 loses
-                player1.send(b'\3\2')
-                player2.send(b'\3\0')
-            elif result == 1: #player 2 loses
-                player1.send(b'\3\0')
-                player2.send(b'\3\2')
-            else:
-                logging.debug("Game result error, got: %d", result)
-                kill_game([player1, player2])
-                return
-            game_round += 1
+        result = compare_cards(p1_card, p2_card)
+        if result == 0:
+            player1.writer.write(b'\3\1')
+            player2.writer.write(b'\3\1')
+        elif result == -1: #player 1 loses
+            player1.writer.write(b'\3\2')
+            player2.writer.write(b'\3\0')
+        elif result == 1: #player 2 loses
+            player1.writer.write(b'\3\0')
+            player2.writer.write(b'\3\2')
+        else:
+            logging.debug("Game result error, got: %d", result)
+            kill_game([player1, player2])
+            return
+        game_round += 1
     kill_game([player1, player2])
-    #**********************************************************************(probably gonna need it's own return though)
     return
+    """
 
 async def limit_client(host, port, loop, sem):
     """
@@ -236,7 +231,7 @@ async def client(host, port, loop):
             result = "lost"
         else:
             result = "drew"
-        logging.debug("Game complete, I %s", result)
+        logging.debug("Game complete,  I %s", result)
         writer.close()
         return 1
     except ConnectionResetError:
@@ -255,15 +250,21 @@ def main(args):
     """
     host = args[1]
     port = int(args[2])
+    loop = asyncio.get_event_loop()
     if args[0] == "server":
+        coroutine = asyncio.start_server(serve_game, host=host, port=port, loop=loop)
+        server = loop.run_until_complete(coroutine)
         try:
+            loop.run_forever()
             # your server should serve clients until the user presses ctrl+c
-            serve_game(host, port)
+            #serve_game(host, port)
         except KeyboardInterrupt:
+            server.close()
+            loop.run_until_complete(server.wait_closed())
             pass
         return
-    else:
-        loop = asyncio.get_event_loop()
+    #else:
+        #loop = asyncio.get_event_loop()
 
     if args[0] == "client":
         loop.run_until_complete(client(host, port, loop))
@@ -284,7 +285,6 @@ def main(args):
         res = loop.run_until_complete(
             asyncio.Task(run_all_clients(), loop=loop))
         logging.info("%d completed clients", res)
-
     loop.close()
 
 if __name__ == "__main__":
